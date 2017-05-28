0e6264b47731c6f9c5efb8aa802dd325b0d0cbd6
Yuval Ishay <yuval@computrade.co.il>
2017-05-28 03:04:00 +0300

    after lesson

--- Section3_Fundamental/src/computrade/constructor/PersonForConstSample.java
+++ Section3_Fundamental/src/computrade/constructor/PersonForConstSample.java
@@ -6,13 +6,54 @@ public class PersonForConstSample {
 	private String lastName;
 	private int id;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		PersonForConstSample person = new PersonForConstSample();
 		System.out.println(person);
-
+		PersonForConstSample person1 = new PersonForConstSample("Yuval");
+		System.out.println(person1);
+		PersonForConstSample person2 = new PersonForConstSample("Yuval","Ishay");
+		System.out.println(person2);
+		PersonForConstSample person3 = new PersonForConstSample("Yuval","Ishay",1234);
+		System.out.println(person3);
+	
 	}
 	
+	public PersonForConstSample() {
+	
+		
+	}
+	
+	public  PersonForConstSample(String firstName) throws Exception {
+		
+		this.firstName = firstName;
+		throw new Exception("from a constructor");
+	
+	}
+	
+	public String PersonForConstSample(String firstName) {
+		
+		this.firstName = firstName;
+		return firstName;
+	}
+
+
+
+	public PersonForConstSample(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	
+
+	public PersonForConstSample(String firstName, String lastName, int id) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.id = id;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
