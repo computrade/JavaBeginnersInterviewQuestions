643e15b07e6c3f6365b5740b162e7475e8ce63ce
Yuval Ishay <yuval@computrade.co.il>
2017-06-16 21:23:56 +0300

    Start Version

--- Section3_Fundamental/src/computrade/exceptions/ExceptionTest.java
+++ Section3_Fundamental/src/computrade/exceptions/ExceptionTest.java
@@ -6,13 +6,13 @@ public class ExceptionTest {
 	   public static void main(String args[]) {
 		   
 		   ExceptionTest exceptionTest = new ExceptionTest();
-		   try {
-			exceptionTest.handleException();
-		   } catch (MyCustomException e) {
-			   // TODO Auto-generated catch block
-			e.printStackTrace();
-		   }
 		   
+//		   try {
+//			exceptionTest.handleException();
+//		   } catch (MyCustomException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		   }
 		   exceptionTest.handleRuntimeException();
 		   
 	     
@@ -23,7 +23,8 @@ public class ExceptionTest {
 		      try {
 		         System.out.println("Access name in element three :" + names[3]);
 		      }catch(ArrayIndexOutOfBoundsException e){
-		    	  throw new MyCustomException("Coverting from un-checked to checked exception: " + e.getMessage());
+		    	  throw new MyCustomException("Coverting from un-checked to checked exception: "
+		    			  + e.getMessage());
 		      }finally {
 		    	 names = null; // Helping the GC
 		         System.out.println("finally of handleException method done!");
