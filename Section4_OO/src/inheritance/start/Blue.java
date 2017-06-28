13f0097719e6f143e47cca99986b581bd6ffd114
Yuval Ishay <yuval@computrade.co.il>
2017-06-28 13:32:22 +0300

    adding setTransparency

--- Section4_OO/src/inheritance/start/Blue.java
+++ Section4_OO/src/inheritance/start/Blue.java
@@ -2,6 +2,8 @@ package inheritance.start;
 
 public class Blue extends Color {
 	
+	private int transparency;
+	
 	public Blue(){
 		System.out.println("Printed from Blue constructor" );
 	}
@@ -9,6 +11,12 @@ public class Blue extends Color {
 	@Override
 	public void fill() {
 		System.out.println("Inside Blue::fill() method.");
+	}
+	
+	@Override
+	public void setTransparency(int transparency){
+		this.transparency = transparency*2;
+		System.out.println("The setTransparency set to: " + this.transparency);
 	}
 	
 }
--- Section4_OO/src/inheritance/start/Green.java
+++ Section4_OO/src/inheritance/start/Green.java
@@ -2,6 +2,8 @@ package inheritance.start;
 
 public class Green extends Color {
 	
+	private int transparency;
+	
 	public Green(){
 		System.out.println("Printed from Green constructor");	
 	}
@@ -9,5 +11,11 @@ public class Green extends Color {
 	@Override
 	public void fill() {
 		System.out.println("Inside Green::fill() method.");
+	}
+	
+	@Override
+	public void setTransparency(int transparency){
+		this.transparency = transparency*2;
+		System.out.println("The setTransparency set to: " + this.transparency);
 	}
 }
