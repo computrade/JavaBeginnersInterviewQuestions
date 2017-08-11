90c322db7bfc446e745e05aa3c9e487e46cdb8cb
Yuval Ishay <yuval@computrade.co.il>
2017-08-11 07:24:27 +0300

    align

--- Section7_Puzzels/src/computrade/puzzels/ReverseNumberExample.java
+++ Section7_Puzzels/src/computrade/puzzels/ReverseNumberExample.java
@@ -14,11 +14,11 @@ public class ReverseNumberExample {
     	
         int reverse = 0;
         int remainder = 0;
+        
         do{
             remainder = number%10;
             reverse = reverse*10 + remainder;
-            number = number/10;
-          
+            number = number/10;         
         }while(number > 0);
       
         return reverse;
